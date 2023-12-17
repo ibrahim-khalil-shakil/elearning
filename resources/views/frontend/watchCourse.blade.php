a95f453cb47321d14a214f38d17ae98ad8fb50cf
Ibrahim Khalil Shakil <iksakil2@gmail.com>
2023-12-17 20:17:05 +0600

    added icons and back url in watchcourse page

--- resources/views/frontend/watchCourse.blade.php
+++ resources/views/frontend/watchCourse.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{asset('public/frontend/src/scss/vendors/plugin/css/star-rating-svg.css')}}" />
     <link rel="stylesheet" href="{{asset('public/frontend/dist/main.css')}}" />
     <link rel="icon" type="image/png" href="{{asset('public/frontend/dist/images/favicon/favicon.png')}}" />
+    <link rel="stylesheet" href="{{asset('public/frontend/fontawesome-free-5.15.4-web/css/all.min.css')}}">
     <style>
         .vjs-poster {
             width: 100%;
@@ -31,42 +32,20 @@
                     </a>
                     <div class="topic-info">
                         <div class="topic-info-arrow">
-                            <a href="#">
-                                <svg width="24" height="25" viewBox="0 0 24 25" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M15.5 19.5L8.5 12.5L15.5 5.5" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
+                            <a href="{{ URL::previous() }}">
+                                <i class="fas fa-chevron-left"></i>
                             </a>
                         </div>
                         <div class="topic-info-text">
                             <h6 class="font-title--xs"><a href="#">{{$course->title_en}}</a></h6>
                             <div class="lesson-hours">
                                 <div class="book-lesson">
-                                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M1.5 2.75H6C6.79565 2.75 7.55871 3.06607 8.12132 3.62868C8.68393 4.19129 9 4.95435 9 5.75V16.25C9 15.6533 8.76295 15.081 8.34099 14.659C7.91903 14.2371 7.34674 14 6.75 14H1.5V2.75Z"
-                                            stroke="#00AF91" stroke-width="1.8" stroke-linecap="round"
-                                            stroke-linejoin="round" />
-                                        <path
-                                            d="M16.5 2.75H12C11.2044 2.75 10.4413 3.06607 9.87868 3.62868C9.31607 4.19129 9 4.95435 9 5.75V16.25C9 15.6533 9.23705 15.081 9.65901 14.659C10.081 14.2371 10.6533 14 11.25 14H16.5V2.75Z"
-                                            stroke="#00AF91" stroke-width="1.8" stroke-linecap="round"
-                                            stroke-linejoin="round" />
-                                    </svg>
-                                    <span>93 Lesson</span>
+                                    <i class="fas fa-book-open text-primary"></i>
+                                    <span>{{$course->lesson?$course->lesson:0}} Lesson</span>
                                 </div>
                                 <div class="totoal-hours">
-                                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M9 17C13.1421 17 16.5 13.6421 16.5 9.5C16.5 5.35786 13.1421 2 9 2C4.85786 2 1.5 5.35786 1.5 9.5C1.5 13.6421 4.85786 17 9 17Z"
-                                            stroke="#FF7A1A" stroke-width="1.8" stroke-linecap="round"
-                                            stroke-linejoin="round" />
-                                        <path d="M9 5V9.5L12 11" stroke="#FF7A1A" stroke-width="1.8"
-                                            stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                    <span>11.5 Hours</span>
+                                    <i class="far fa-clock text-danger"></i>
+                                    <span>{{$course->duration?$course->duration:0}} Hours</span>
                                 </div>
                             </div>
                         </div>
