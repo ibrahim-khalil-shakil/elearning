8a9f89d974a208e250016a4971b1771c7d4de57f
Ibrahim Khalil Shakil <iksakil2@gmail.com>
2023-12-25 01:06:39 +0600

    added popular category courses

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,22 @@ class HomeController extends Controller
         $instructor = Instructor::get();
         $category = CourseCategory::get();
         $popularCourses = Course::where('tag', 'popular')->get();
-        return view('frontend.home', compact('course', 'instructor', 'category', 'popularCourses'));
-    }
 
+        $designCategories = CourseCategory::whereIn('category_name', ['Graphics Desgin', 'Web Design', 'Video Editing', '2D Animation', '3D Animation'])->pluck('id')->toArray();
+        $designCourses = Course::whereIn('course_category_id', $designCategories)->where('tag', 'popular')->get();
+
+        $developmentCategories = CourseCategory::whereIn('category_name', ['Web Development', 'Mobile Development', 'Game Development', 'Database Design & Development', 'Data Science'])->pluck('id')->toArray();
+        $developmentCourses = Course::whereIn('course_category_id', $developmentCategories)->where('tag', 'popular')->get();
+
+        $businessCategories = CourseCategory::whereIn('category_name', ['Digital Marketing', 'Entrepreneurship'])->pluck('id')->toArray();
+        $businessCourses = Course::whereIn('course_category_id', $businessCategories)->where('tag', 'popular')->get();
+
+        $itCategories = CourseCategory::whereIn('category_name', ['Hardware', 'Network Technology', 'Software & Security', 'Operating System & Server'])->pluck('id')->toArray();
+        $itCourses = Course::whereIn('course_category_id', $itCategories)->where('tag', 'popular')->get();
+
+        return view(
+            'frontend.home',
+            compact('course', 'instructor', 'category', 'popularCourses', 'designCourses', 'developmentCourses', 'businessCourses', 'itCourses')
+        );
+    }
 }
